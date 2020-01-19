b050c15f718e4b6b6b755a67a968969c233e364a
R D <ramnik.dahiya@gmail.com>
2020-01-19 17:42:23 +0000

    Added multiplication, tests and circle ci

--- test_calculator.py
+++ test_calculator.py
@@ -9,6 +9,9 @@ class TestCalculator(unittest.TestCase):
     def test_subtraction(self):
         assert 2 == calculator.subtract(4, 2)
 
+    def test_multiplication(self):
+        assert 100 == calculator.multiply(10, 10)
+
 
 if __name__ == '__main__':
     unittest.main()
